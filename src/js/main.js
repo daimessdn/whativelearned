e83814b737c9a560dd1f4c09d84f5a69f5c48b9d
daimessdn@gmail.com <daimessdn@gmail.com>
2021-01-02 20:07:32 +0700

    changed serviceWorker origin

--- src/js/main.js
+++ src/js/main.js
@@ -43,7 +43,7 @@ const generateQuotes = () => {
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
       navigator.serviceWorker
-        .register("../serviceWorker.js")
+        .register("./serviceWorker.js")
         .then(res => console.log("service worker registered"))
         .catch(err => console.log("service worker not registered", err))
     })
